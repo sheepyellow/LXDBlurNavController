f2fb47f93a969de516b53e6b280aa864d1584613
Sindri Lin <634893985@qq.com>
2015-10-29 12:22:19 +0800

    Initial Commit

---
+++ LXDBlurNavController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXDBlurNavController
+//
+//  Created by 林欣达 on 15/10/29.
+//  Copyright © 2015年 sindri lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXDBlurNavController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXDBlurNavController
+//
+//  Created by 林欣达 on 15/10/29.
+//  Copyright © 2015年 sindri lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
